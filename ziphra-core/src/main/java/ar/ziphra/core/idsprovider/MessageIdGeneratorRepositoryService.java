bcd1db5e107f53081ab1b2237c28c0ca5fbc1e46
Jorge Kagiagian <jorgekagiagian@gmail.com>
2025-07-07 16:52:31 -0300

    stand alone

--- ziphra-core/src/main/java/ar/ziphra/core/idsprovider/MessageIdGeneratorRepositoryService.java
+++ ziphra-core/src/main/java/ar/ziphra/core/idsprovider/MessageIdGeneratorRepositoryService.java
@@ -3,12 +3,13 @@ package ar.ziphra.core.idsprovider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import ar.ziphra.commonback.common.interfaces.KeyLockerCallback;
 import ar.ziphra.core.repository.MessageRepository;
 
-@Service
+//@Service  esto es solo para cuando se usa el id dentro del server
 @Qualifier("MessageIdGeneratorRepositoryService")
 public class MessageIdGeneratorRepositoryService extends MessageIdGeneratorAbstractService{
 	@Autowired @Lazy
